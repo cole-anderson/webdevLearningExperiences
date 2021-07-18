c8908ddb9a23cb5f844be6e4bf4b110e5618175e
cole <spedits@gmail.com>
2021-07-17 21:31:54 -0600

    login screen

--- classicTodo/todo/src/Components/Todo/TodoApp.jsx
+++ classicTodo/todo/src/Components/Todo/TodoApp.jsx
@@ -5,9 +5,49 @@ class TodoApp extends Component {
         return (
             <div className="TodoApp">
                 My Todo
+                <LoginComponent />
             </div>
         )
     }
 }
+
+class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.handleUserPassChange = this.handleUserPassChange.bind(this);
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    // handleUsernameChange(event) {
+    //     this.setState({ username: event.target.value });
+    // }
+
+    // handlePasswordChange(event) {
+    //     this.setState({ password: event.target.value });
+    // }
+    handleUserPassChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUserPassChange} />
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleUserPassChange} />
+                <button> Login </button>
+            </div>
+        )
+    };
+}
+
 
 export default TodoApp;
