a182161e7d8e6f80ccabf02bcee7d21663edb38a
sinpulse <spedits@gmail.com>
2021-08-30 20:40:10 -0600

    started backend

---
+++ classicTodo/rest-web-service/src/main/java/com/northernlightsdev/restwebservice/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.northernlightsdev.restwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller: handling http requests
+
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world
+	//method returns hello world
+	
+	
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
