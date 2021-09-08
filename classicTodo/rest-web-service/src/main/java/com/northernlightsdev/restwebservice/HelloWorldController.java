2b60fbdf720ce2849a343ea21bde9c95539e6598
sinpulse <spedits@gmail.com>
2021-09-07 20:51:56 -0600

    rest

--- classicTodo/rest-web-service/src/main/java/com/northernlightsdev/restwebservice/HelloWorldController.java
+++ classicTodo/rest-web-service/src/main/java/com/northernlightsdev/restwebservice/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.northernlightsdev.restwebservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller: handling http requests
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 	
 	//GET
@@ -24,6 +27,12 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	//hello-world/path-variable/cole
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPath(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
 }
