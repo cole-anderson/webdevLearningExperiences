10718b5836c29cd0a51da7f73d315066628e73a1
cole <spedits@gmail.com>
2021-09-12 19:46:48 -0600

    more api work

--- classicTodo/todo/src/Components/Todo/WelcomeComponent.jsx
+++ classicTodo/todo/src/Components/Todo/WelcomeComponent.jsx
@@ -10,8 +10,10 @@ class WelcomeComponent extends Component {
         super(props)
         this.retreiveWelcomeMessage = this.retreiveWelcomeMessage.bind(this);
         this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
+        this.handleErrorResponse = this.handleErrorResponse.bind(this);
         this.state = {
             welcomeMessage: '',
+            error: '',
         }
     }
 
@@ -36,13 +38,27 @@ class WelcomeComponent extends Component {
     };
 
     retreiveWelcomeMessage() {
-        HelloWorldService.executeHelloWorldService()
+        // HelloWorldService.executeHelloWorldService()
+        //     .then(response => this.handleSuccessfulResponse(response))
+        // //.catch()
+
+        // HelloWorldService.executeHelloWorldBeanService()
+        //     .then(response => this.handleSuccessfulResponse(response))
+        // //.catch()
+
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.match.params.name)
             .then(response => this.handleSuccessfulResponse(response))
-        //.catch()
+            .catch(error => this.handleErrorResponse(error.response.data.message))
     }
 
     handleSuccessfulResponse(response) {
-        this.setState({ welcomeMessage: response.data });
+        console.log(response);
+        this.setState({ welcomeMessage: response.data.message });
+    }
+
+    handleErrorResponse(error) {
+        console.log(error);
+        this.setState({ welcomeMessage: 'error occurred' });
     }
 }
 export default WelcomeComponent;
--- classicTodo/todo/src/Components/api/todo/HelloWorldService.js
+++ classicTodo/todo/src/Components/api/todo/HelloWorldService.js
@@ -6,6 +6,16 @@ class HelloWorldService {
         return axios.get('http://localhost:8080/hello-world'); //promise
 
     };
+    executeHelloWorldBeanService() {
+        // console.log('executed service');
+        return axios.get('http://localhost:8080/hello-world-bean'); //promise
+
+    };
+    executeHelloWorldPathVariableService(name) {
+        // console.log('executed service');
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`); //promise
+
+    };
 
 }
 export default new HelloWorldService();
