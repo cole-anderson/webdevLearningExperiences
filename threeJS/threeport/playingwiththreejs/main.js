905a252836a13fc0fbaaa358e4a92ce42f67211c
cole <spedits@gmail.com>
2022-01-11 21:03:46 -0700

    3710 was the best course ever

--- threeJS/threeport/playingwiththreejs/main.js
+++ threeJS/threeport/playingwiththreejs/main.js
@@ -16,17 +16,41 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 15, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });//no light req
+// const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });//no light req
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material);
 
+//Lighting:
+const pointLight = new THREE.PointLight(0xfffff);
+pointLight.position.set(20, 15, 0);
+
+// const ambientLight = new THREE.AmbientLight(0xfffff);
+
+const lightHelp = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelp);
+
+
+//Add objects to scene:
+scene.add(pointLight, /*ambientLight*/);
 scene.add(torus);
 
+
+
+//Animation Loop(Main)
 function animate() {
   requestAnimationFrame(animate);
+  document.onkeydown = function (e) {
+    switch (e.keyCode) {
+      case 37:
+        torus.rotation.x += 0.1;
+        break;
+    }
+  }
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  //Rotate Animation:
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.005;
+  // torus.rotation.z += 0.01;
 
   renderer.render(scene, camera);
 }
